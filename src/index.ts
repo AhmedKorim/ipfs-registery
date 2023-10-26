6ec6586f3478ea79924fc5ca42e64d40dde2944a
ahmed <ahmedkorrim@gmail.com>
2023-10-26 23:38:14 +0200

    Issues with esm

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,24 @@ import {
   DEFAULT_RETURN_FORMAT,
   eth,
   PayableCallOptions,
-  TransactionReceipt,
+  // TransactionReceipt,
   Web3Context,
   Web3PluginBase,
+  validator,
 } from "web3";
 import { Helia } from "helia";
 import { dagCbor } from "@helia/dag-cbor";
+
 import { DEPLOYED_AT, registryAbi, RegistryAbiInterface } from "./registry-abi";
-import { isAddress } from "web3-validator";
+
 export type IpfsRegistryConfig = {
+  // The abi for the registry contract
   registryContractAbi?: RegistryAbiInterface;
+  // Blocknumber when the contract was created
   registryContractDeployedAt?: bigint;
+  // Registry contract address
   registryContractAddress?: Address;
-  // Helia node for cleanup
+  // Helia IPFS node
   heliaNode: Helia;
 };
 
@@ -30,15 +35,22 @@ export class IpfsRegistry extends Web3PluginBase {
   private readonly _registryContract: Contract<RegistryAbiInterface>;
   private readonly _registryContractDeployedAt: bigint;
   private readonly _helia: Helia;
+
   constructor(config: IpfsRegistryConfig) {
     super();
 
-    this._registryContractDeployedAt = config.registryContractDeployedAt || DEPLOYED_AT;
+    this._registryContractDeployedAt =
+      config.registryContractDeployedAt || DEPLOYED_AT;
     const contractAbi = config.registryContractAbi || registryAbi;
-    const contractAddress: Address = config.registryContractAddress || "0xA683BF985BC560c5dc99e8F33f3340d1e53736EB";
+    const contractAddress: Address =
+      config.registryContractAddress ||
+      "0xA683BF985BC560c5dc99e8F33f3340d1e53736EB";
 
     // Instantiate the registry contract
-    this._registryContract = new Contract<RegistryAbiInterface>(contractAbi, contractAddress);
+    this._registryContract = new Contract<RegistryAbiInterface>(
+      contractAbi,
+      contractAddress
+    );
     // Linking web3 context the registry contract
     this._registryContract.link(this);
 
@@ -47,9 +59,9 @@ export class IpfsRegistry extends Web3PluginBase {
 
   /**
    * Upload a blob to ipfs and return the CID for it
-   * @param{fileData} - Represents the bytes for the file to upload
+   * @param fileData - Represents the bytes for the file to upload
    * */
-  private async uploadFileToIpfs(fileData: Uint8Array): Promise<string> {
+  public async uploadFileToIpfs(fileData: Uint8Array): Promise<string> {
     const dag = dagCbor(this._helia);
     const cid = await dag.add(fileData);
     return cid.toString();
@@ -57,18 +69,20 @@ export class IpfsRegistry extends Web3PluginBase {
 
   /**
    * Upload file to ipfs and submit the CID to the registry contract
-   *
+   * @param fileData - Represents the bytes for the file to upload
+   * @param txOptions - Payable options that's used for configuring the transaction
    * */
   public async uploadFileAndRegister(
-    fileData: Uint8Array,
-    txOptions: PayableCallOptions,
+    fileCid: string,
+    txOptions: PayableCallOptions
   ): Promise<IpfsRegistryResponse> {
-    // Upload the file to IPFS
-    const fileCid = await this.uploadFileToIpfs(fileData);
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    const txReceipt: TransactionReceipt = await this._registryContract.methods.store(fileCid).send(txOptions);
+  /*  const _txReceipt: TransactionReceipt =*/
+    await this._registryContract.methods
+      .store(fileCid)
+      .send(txOptions);
     return {
-      transactionHash: txReceipt.transactionHash.toString(),
+      transactionHash: "txReceipt.transactionHash.toString()",
       uploadedCID: fileCid,
     };
   }
@@ -85,14 +99,18 @@ export class IpfsRegistry extends Web3PluginBase {
    * Combine all chunks in one list
    * */
   public async getCIDsOfAddress(address: string): Promise<string[]> {
-    if (!isAddress(address)) {
+    // Check for address validly
+    if (!validator.isAddress(address)) {
       throw new Error("Invalid address");
     }
     // The full list of cids stored in the contract
     const cids: string[] = [];
 
     // get the latest block from the chain
-    const lastBlockNumber = await eth.getBlockNumber(this, DEFAULT_RETURN_FORMAT);
+    const lastBlockNumber = await eth.getBlockNumber(
+      this,
+      DEFAULT_RETURN_FORMAT
+    );
 
     for (
       let currentBlock = this._registryContractDeployedAt;
@@ -100,13 +118,16 @@ export class IpfsRegistry extends Web3PluginBase {
       // 1024 as this is maximum entries per query
       currentBlock += BigInt(1024)
     ) {
-      const fetchedEvents = await this._registryContract.getPastEvents("CIDStored", {
-        fromBlock: currentBlock,
-        toBlock: currentBlock + BigInt(1024),
-        filter: {
-          owner: address,
-        },
-      });
+      const fetchedEvents = await this._registryContract.getPastEvents(
+        "CIDStored",
+        {
+          fromBlock: currentBlock,
+          toBlock: currentBlock + BigInt(1024),
+          filter: {
+            owner: address,
+          },
+        }
+      );
       // Looping over the events as the event parameter `cid` entry is an index parameter not the
       // actual CID value submitted to the store call
 
@@ -115,14 +136,18 @@ export class IpfsRegistry extends Web3PluginBase {
           continue;
         }
         const transactionHash = event.transactionHash;
-        const transaction = await eth.getTransaction(this, transactionHash, DEFAULT_RETURN_FORMAT);
+        const transaction = await eth.getTransaction(
+          this,
+          transactionHash,
+          DEFAULT_RETURN_FORMAT
+        );
         if (!transaction) {
           continue;
         }
         // Decoding the transaction parameters from the input
         const txInput = eth.abi.decodeParameters(
           [{ internalType: "string", name: "cid", type: "string" }],
-          transaction.input.slice(10),
+          transaction.input.slice(10)
         );
         const cid = txInput.cid as string;
         cids.push(cid);
---
+++ test/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: ".",
+  testEnvironment: "node",
+  testRegex: ".test.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  preset: "ts-jest/presets/default-esm",
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.[jt]s$": "$1",
+  },
+};
